3d431ea306a9284820186c29953e0763abbacaa5
08081108 <bruebirdsora8@gmail.com>
2026-07-14 10:32:02 +0900

    FactoryBot設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { Faker::Name.name }
+    email                 { Faker::Internet.email }
+    password              { Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    last_name             { '山田' }
+    first_name            { '太朗' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    date_birth            { Faker::Date.birthday(min_age: 18, max_age: 80) }
   end
 end
