f7fa237da24d94c72cf88b6e76ff2e98678efb6d
08081108 <bruebirdsora8@gmail.com>
2026-08-04 15:13:50 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,32 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :condition
+    validates :shipping_fee_id
+    validates :prefecture
+    validates :shipping_day_id
+    validates :price, numericality: { only_integer: true, message: 'Half-width number' }
+    validates :price,
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                              message: 'is out of setting range' }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition
+    validates :shipping_fee_id
+    validates :prefecture
+    validates :shipping_day_id
+  end
 end
